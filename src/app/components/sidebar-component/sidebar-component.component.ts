200b2971ed7f05848cb6780494f83ff049c9a3f2
pedromegliato <pedrao.gabriel1@gmail.com>
2022-12-08 16:59:31 -0400

    correcao menu

--- src/app/components/sidebar-component/sidebar-component.component.ts
+++ src/app/components/sidebar-component/sidebar-component.component.ts
@@ -9,23 +9,55 @@ import { Router } from '@angular/router';
 export class SidebarComponentComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  ids: any;
+  
   ngOnInit(): void {
+      this.setLoadMenuActivePage();
+  }
+
+  setLoadMenuActivePage = () => {
+      if(window.location.pathname == '/admin/dashboard') {
+          this.ids = 1;
+      } else if(window.location.pathname == '/admin/veiculos'){
+          this.ids = 2;
+      } else if(window.location.pathname == '/admin/servicos'){
+          this.ids = 3;
+      } else if(window.location.pathname == '/admin/configuracao'){
+          this.ids = 4;
+      }
+  }
+
+  setmenuActive = (event: any) => {
+      if(event === 1){
+          this.ids = 1;
+      } else if(event === 2){
+          this.ids = 2;
+      } else if(event === 3){
+          this.ids = 3;
+      } else if(event === 4){
+          this.ids = 4;
+      }
   }
 
   rota = (event :any) => {
-    switch (event) {
-      case 1:
-          this.router.navigate(['/admin/dashboard']);
-          break;
-      case 2:
-          this.router.navigate(['/admin/veiculos']);
-          break;
-      case 2:
-          this.router.navigate(['/admin/veiculos']);
-          break;
+      switch (event) {
+        case 1:
+            this.setmenuActive(event);
+            this.router.navigate(['/admin/dashboard']);
+            break;
+        case 2:
+            this.setmenuActive(event);
+            this.router.navigate(['/admin/veiculos']);
+            break;
+        case 3:
+            this.setmenuActive(event);
+            this.router.navigate(['/admin/servicos']);
+            break;
+        case 4:
+            this.setmenuActive(event);
+            this.router.navigate(['/admin/configuracao']);
+            break;
       }
-    }
   }
 
 }
